9aa832b4c842fb502c68e848bca63a6f8c2a6f8e
a104171 <a104171@alunos.uminho.pt>
2023-02-28 18:11:54 +0000

    2ª semana finished

--- 2semana/C.c
+++ 2semana/C.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
-int main(){
+int main(){ 
+    int n, vitoriasInzel = 0, vitoriasCMD = 0, empates = 0;
+    char jogadaInzel, jogadaCMD;
+
+    if (scanf("%d",&n) == 1){}
+
+    for (int i = 0; i < n; i++){
+        if (scanf(" %c", &jogadaInzel) == 1){}
+        if (scanf(" %c", &jogadaCMD) == 1){}
+
+        if ((jogadaInzel == '@' && jogadaCMD == '+') || (jogadaInzel == '|' && jogadaCMD == '*') || (jogadaInzel == 'X' && jogadaCMD == '-')){
+            vitoriasInzel++;
+        }
+        else if ((jogadaInzel == 'X' && jogadaCMD == '*') || (jogadaInzel == '@' && jogadaCMD == '-') || (jogadaInzel == '|' && jogadaCMD == '+')){
+            vitoriasCMD++;
+        }
+        else{
+            empates++;
+        }
+    }
+
+    printf("%d %d %d\n", vitoriasInzel, vitoriasCMD, empates);
     return 0;
 }
