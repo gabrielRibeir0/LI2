d442a53d5c7e1cf7af8574230d54f77af36dac1d
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2023-03-09 13:49:37 +0000

    Semana 3 - Exercícios B e C

--- 3semana/b.c
+++ 3semana/b.c
@@ -1,3 +1,54 @@
-int main() {
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  int n, num_comandos, pos = 5;
+  char comandos[21];
+
+  //Ler o número de linhas
+  if(scanf("%d", &n) != 1){
+    printf("Erro na leitura");
+    return 1;
+  }
+  
+  int codigo[n];
+
+  //Ciclo para ler e processar cada linha de comandos
+  for(int i = 0; i < n; i++){
+    if(scanf("%s", comandos) != 1){
+      printf("Erro na leitura");
+      return 1;
+    }
+
+    num_comandos = strlen(comandos);
+    
+    //Processamento de comando a comando atualizando a posição
+    for(int j = 0; j < num_comandos; j++){
+      if(comandos[j] == 'C'){
+        if(pos < 7)
+          pos += 3;
+      }
+      else if(comandos[j] == 'B'){
+        if(pos > 3)
+          pos -= 3;
+      }
+      else if(comandos[j] == 'D'){
+        if(pos % 3 != 0)
+          pos++;
+      }
+      else if(comandos[j] == 'E'){
+        if(pos % 3 != 1)
+          pos--;
+      }
+    }
+    codigo[i] = pos;      //Guardar o código dígito a dígito num array
+  }
+
+  //Escrever o código no final
+  for(int i = 0; i < n; i++){
+    printf("%d", codigo[i]);
+  }
+  putchar('\n');
+
   return 0;
 }
--- 3semana/c.c
+++ 3semana/c.c
@@ -1,3 +1,50 @@
+#include <stdio.h>
+//USAR ARRAYS PORQUE EU LI MAL O ENUNCIADO PRONTO É ISSO
 int main(){
+    int n, k, visivel,contador = 0;
+
+    if(scanf("%d", &n) != 1){
+        printf("Erro na leitura");
+        return 1;
+    }
+
+    int valores[n];
+
+    for(int i = 0; i < n; i++){
+        if(scanf("%d", &k) != 1){
+            printf("Erro na leitura");
+            return 1;
+        }
+
+        int alturas[k];
+
+        for(int j = 0; j < k; j++){
+            if(scanf(" %d", &alturas[j]) != 1){
+                printf("Erro na leitura");
+                return 1;
+            }
+        }
+
+        for(int j = 0; j < k; j++){
+            visivel = 1;
+
+            for(int v  = 0; v < j; v++){
+                if(alturas[j] <= alturas[v]){
+                    visivel = 0;
+                    break;
+                }
+            }
+            
+            if(visivel == 1)
+                contador++;
+        }
+        
+        valores[i] = contador;
+        contador = 0;
+    }
+
+    for(int i = 0; i < n; i++){
+        printf("%d\n",valores[i]);
+    }
     return 0;
 }
