49d97dc3b2f22527773897e8b67850f529c68d7a
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2023-03-15 19:59:33 +0000

    4ª semana - Exercício B e início C

--- 4semana/b.c
+++ 4semana/b.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
+    int n, compLinha, encontrado;
+    char linha[1000]; 
+
+    if(scanf("%d", &n) != 1){
+        return 1;
+    }
+
+    char letras[n+1];
+    letras[n] = '\0';
+
+    for(int i = 0; i < n; i++){
+        if(scanf("%s", linha) != 1){
+            return 1;
+        }
+
+        compLinha = strlen(linha);
+        encontrado = 0;
+
+        for(int j = 0; !encontrado && j < compLinha; j++){
+            for(int k = j + 1; !encontrado && k < compLinha; k++){
+                if(linha[j] == linha[k]){
+                    letras[i] = linha[j];
+                    encontrado = 1;
+                }
+            }
+        }  
+    }
+
+    printf("%s\n",letras);
     return 0;
 }
--- 4semana/c.c
+++ 4semana/c.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
+    int n, x, j, compLinha, diff;
+    char linha[1000];
+
+    if(scanf("%d",&n) != 1){
+        return 1;
+    }
+
+    int posicoes[n];
+
+    for(int i = 0; i < n; i++){
+        if(scanf("%d", &x) != 1){
+            return 0;
+        }
+
+        if(scanf("%s", linha) != 1){
+            return 1;
+        }
+
+        compLinha = strlen(linha);
+        diff = 1;
+
+        for(j = 0; j < compLinha; j++){
+            for(int k = j + 1; k < j + x; k++){
+                if(linha[k] == linha[k-1]){
+                    diff = 0;
+                    break;
+                }
+            }
+            if(diff){
+                posicoes[i] = j;
+                break;
+            }
+        }
+        if(!diff)
+            posicoes[i] = -1;
+    }
+
+    for(int w = 0; w < n; w++)
+        printf("%d\n", posicoes[w]);
     return 0;
 }
