bcff583fba9f3da8ea00a55f7512780df8de4aaa
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2023-03-21 21:23:20 +0000

    aaaaaaaaaaa

--- 5semana/a.c
+++ 5semana/a.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    int n, k, guicheAtual, tamanhoSucc, loop, proximoGuiche, tamanho;
+    int n, k, primeiroGuiche, loop, proximoGuiche, tamanho;
+    int guiches[100];
 
     if(scanf("%d", &n) != 1){
         return 1;
     }
+
+    int guichesPassados[n][101];
 
     for(int i = 0; i < n; i++){
         if(scanf("%d", &k) != 1){
             return 1;
         }
 
-        int guiches[k];
-        int guichesPassados[k];
 
-        if(scanf("%d", &guicheAtual) != 1){
+        if(scanf("%d", &primeiroGuiche) != 1){
             return 1;
         }
 
@@ -25,31 +26,47 @@ int main(){
             }
         }
 
-        guichesPassados[0] = guicheAtual;
+        guichesPassados[i][0] = primeiroGuiche;
+
         tamanho = loop = 0;
         while(!loop){
-            proximoGuiche = guiches[guichesPassados[tamanho] - 1];
+            proximoGuiche = guiches[guichesPassados[i][tamanho] - 1];
 
             if(proximoGuiche == 0)
                 break;
             
-            tamanho++;
-            
             for(int w = 0; w < tamanho; w++){
-                if(guichesPassados[w] == proximoGuiche){
+                if(guichesPassados[i][w] == proximoGuiche){
                     loop = 1;
                     break;
                 }
             }
+
+            tamanho++;
+
+            if(loop){
+                guichesPassados[i][tamanho] = -1;
+            }
+            else{ 
+                guichesPassados[i][tamanho] = proximoGuiche;
+                guichesPassados[i][tamanho + 1] = -2;
+            }
         }
+    }
 
-        for(int z = 0; z < tamanho; z++){
-            printf("%d ", guichesPassados[z]);
+    for(int l = 0; l < n; l++){
+        for(int z = 0; guichesPassados[l][z] != -2; z++){
+            if(guichesPassados[l][z] == -1){
+                printf("CICLO INFERNAL");
+                break;
+            }
+            else{
+                if(guichesPassados[l][z+1] == -2)
+                    printf("%d", guichesPassados[l][z]);
+                else
+                    printf("%d ", guichesPassados[l][z]);
+            }
         }
-
-        if(loop)
-            printf("CICLO INFERNAL ");
-
         putchar('\n');
     }
     return 0;
