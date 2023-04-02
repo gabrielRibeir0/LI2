96f2cbf058c8522237596963ca41d3b49e81b535
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2023-04-02 17:20:47 +0100

    funciona mas excede o limite de tempo

--- 6semana/d.c
+++ 6semana/d.c
@@ -7,14 +7,7 @@ typedef struct pedido{
     int lucro;
 } pedido;
 
-int estaNaLista(int lista[], int n, int x){
-    for(int i = 0; i < n; i++){
-        if(lista[i] == x)
-            return 1;
-    }
-    return 0;
-}
-
+//função para calcular o maior elemento numa lista (usada para calcular o maior lucro)
 int maxLista(int lista[], int n){
     int max = 0;
     for(int i = 0; i < n; i++){
@@ -25,38 +18,47 @@ int maxLista(int lista[], int n){
     return max;
 }
 
-int lucroMax( pedido listaPedidos[], int pedidoAnterior, int tempo, int lucroAcumulado, int pedidosPassados[], int numPedidos, int contagemPedidos){
-    //se a sequência do caminho já tiver o num de pedidos, quer dizer que chegamos ao fim das combinações e devolve o lucro acumulado (caso paragem)
-    if(contagemPedidos == numPedidos)
-        return lucroAcumulado;
+int calculaLucro(pedido listaPedidos[], int numPedidos, int tempoAcumulado, int lucroAcumulado, int pedidosPossiveis[]){
+    int listaLucros[numPedidos]; // array para guardar os lucros que se consegue fazendo cada pedido
+    int copiaPedidos[numPedidos]; // array para fazer uma cópia dos pedidos possíveis, de forma a não alterar o array original
 
-    //se for a primeira chamada da função
-    if(contagemPedidos == 0){
-        pedidosPassados[0] = pedidoAnterior;
-        contagemPedidos++;
-    }
-
-    int listaLucro[numPedidos - contagemPedidos]; //só é preciso guardar os lucros de n - contagem caminhos, os que já passamos não é preciso contar
-    int j = 0;
+    //inicializar os lucros de cada pedido com um número baixo
     for(int i = 0; i < numPedidos; i++){
-        if(estaNaLista(pedidosPassados, contagemPedidos, listaPedidos[i].id))
-            continue;
-
-        pedidosPassados[contagemPedidos] = listaPedidos[i].id;
-        
-        if(listaPedidos[i].tempoEntrega >= tempo + listaPedidos[i].tempoConfecao){
-            listaLucro[j] = lucroMax(listaPedidos, listaPedidos[i].id, listaPedidos[i].tempoEntrega, listaPedidos[i].lucro + lucroAcumulado, pedidosPassados, numPedidos, contagemPedidos + 1);
-            j++;
-        }
-
+        listaLucros[i] = -1;
     }
-    
-    if(j == 0)
-        return 0;
-    else
-        return maxLista(listaLucro, j);
+
+    int cont = 0;
+
+    for(int i = 0; i < numPedidos; i++){
+        //alterar a possibilidade do pedido em função do tempo e se já foi feito e aumentar o contador
+        if(pedidosPossiveis[i] && listaPedidos[i].tempoEntrega >= tempoAcumulado + listaPedidos[i].tempoConfecao){
+            pedidosPossiveis[i] = 1;
+            cont++;
+        }
+        else{
+            pedidosPossiveis[i] = 0;
+        }
+    }
+
+    if(cont == 0){ //se o contador de pedidos possíveis for 0, devolver o lucro que foi acumulado
+        return lucroAcumulado;
+    }
+    else{
+        for(int i = 0; i < numPedidos; i++){
+            if(pedidosPossiveis[i]){ //se o pedido for possível calcular o lucro ao fazer esse pedido (max das combinações para a frente) e guardar no array
+                for(int j = 0; j < numPedidos; j++){
+                    copiaPedidos[j] = pedidosPossiveis[i];
+                }
+                copiaPedidos[i] = 0;
+                listaLucros[i] = calculaLucro(listaPedidos, numPedidos, listaPedidos[i].tempoEntrega, lucroAcumulado + listaPedidos[i].lucro, copiaPedidos);
+            }
+            
+        }
+    }
+
+    return maxLista(listaLucros, numPedidos); //return do maior lucro da lista (i.e maior lucro a partir de todas as combinações daqui para a frente)
 }
- 
+
 int main(){
     int n;
 
@@ -72,15 +74,19 @@ int main(){
         }
     }
 
-    int contagemPedidos = 0;
-    int listaLucros[n];
-    int pedidosPassados[n];
+    int listaLucros[n]; //array para guardar o maior lucro começando por fazer cada um dos pedidos
+    int pedidosPossiveis[n]; //pedidos que é possível realizar, 1 se sim, 0 se não (é 0 se o pedido já tiver sido concluído ou se não tiver tempo)
     for(int i = 0; i < n; i++){
-        listaLucros[i] = lucroMax(listaPedidos, listaPedidos[i].id, listaPedidos[i].tempoEntrega, listaPedidos[i].lucro, pedidosPassados, n, contagemPedidos);
+        //inicializar todos os pedidos como possíveis
+        for(int j = 0; j < n; j++){
+            pedidosPossiveis[j] = 1;
+        }
+        pedidosPossiveis[i] = 0; //o pedido em que começa deixa de ser possível fazer de novo
+
+        //guardar o lucro máximo possível ao começar neste pedido no array
+        listaLucros[i] = calculaLucro(listaPedidos, n, listaPedidos[i].tempoEntrega, listaPedidos[i].lucro, pedidosPossiveis);
     }
 
-
-    printf("%d\n", maxLista(listaLucros, n));
-
+    printf("%d\n", maxLista(listaLucros,n)); //print do maior lucro
     return 0;
 }
